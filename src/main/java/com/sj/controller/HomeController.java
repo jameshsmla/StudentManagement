0b86f75190e4eed5ff48bba8767af348dcb7f0e1
James <James@PT-MIL-072.Pratian.com>
2018-03-21 17:45:45 +0530

    Layout

---
+++ src/main/java/com/sj/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.sj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView homePage() {
+		System.out.println("Home Page");
+		ModelAndView mv=new ModelAndView("index");
+		return mv;
+	}
+
+}
