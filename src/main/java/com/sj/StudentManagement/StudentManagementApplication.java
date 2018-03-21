3eff62346a0e45b374d378ffde5e008ccfe38d03
james <shamalajames@gmail.com>
2018-03-21 22:31:24 +0530

    view resol

--- src/main/java/com/sj/StudentManagement/StudentManagementApplication.java
+++ src/main/java/com/sj/StudentManagement/StudentManagementApplication.java
@@ -2,8 +2,10 @@ package com.sj.StudentManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.sj")
 public class StudentManagementApplication {
 
 	public static void main(String[] args) {
